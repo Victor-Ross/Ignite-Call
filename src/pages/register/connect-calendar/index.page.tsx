57ba6b150f75762945337fedb153677f4eb59719
Victor Ross <victor2ross@gmail.com>
2023-03-10 21:31:21 -0300

    Next auth adapter for Prisma from zero

---
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -0,0 +1,66 @@
+// import { api } from '@/src/lib/axios';
+import { signIn, useSession } from 'next-auth/react';
+import { Button, Heading, MultiStep, Text } from '@ignite-ui/react';
+
+import { ArrowRight, Check } from 'phosphor-react';
+
+import { Container, Header } from '../styles';
+import { AuthError, ConnectBox, ConnectItem } from './styles';
+import { useRouter } from 'next/router';
+
+export default function Register() {
+  const session = useSession();
+  const router = useRouter();
+
+  const hasAuthError = !!router.query.error;
+  const isSignedIn = session.status === 'authenticated';
+
+  async function handleConnectCalendar() {
+    await signIn('google');
+  }
+
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Conecte sua agenda!</Heading>
+        <Text>
+          conecte o seu calendário para verificar automaticamente as horas
+          ocupadas e os novos eventos à medida em que são agendados.
+        </Text>
+
+        <MultiStep size={4} currentStep={2} />
+      </Header>
+
+      <ConnectBox>
+        <ConnectItem>
+          <Text>Google Calendar</Text>
+          {isSignedIn ? (
+            <Button variant="secondary" size="sm" disabled>
+              Conectado
+              <Check />
+            </Button>
+          ) : (
+            <Button
+              variant="secondary"
+              size="sm"
+              onClick={handleConnectCalendar}
+            >
+              Conectar
+              <ArrowRight />
+            </Button>
+          )}
+        </ConnectItem>
+        {hasAuthError && (
+          <AuthError size="sm">
+            Falha ao conectar-se ao Google, verifique se você habilitou as
+            permissões de acesso ao Google Calendar.
+          </AuthError>
+        )}
+        <Button disabled={!isSignedIn}>
+          Próximo passo
+          <ArrowRight />
+        </Button>
+      </ConnectBox>
+    </Container>
+  );
+}
